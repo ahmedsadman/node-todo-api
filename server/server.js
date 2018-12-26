c487356c2f09b56e609592c892302dd6fbc39ddd
Ahmed Sadman <ahmedsadman.211@gmail.com>
2018-12-26 20:54:37 +0600

    delete todo route

--- server/server.js
+++ server/server.js
@@ -53,6 +53,21 @@ app.get('/todos/:id', (req, res) => {
         });
 });
 
+app.delete('/todos/:id', (req, res) => {
+    const { id } = req.params;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id)
+        .then((todo) => {
+            if (!todo) return res.status(404).send();
+            res.send(todo);
+        })
+        .catch(err => res.status(400).send(err));
+});
+
 // ------------- SERVER SETTINGS -------------
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
